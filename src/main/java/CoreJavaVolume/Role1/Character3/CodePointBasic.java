ec169401404326f42b720e8d17a3e9670a3cc06b
ACtangle <674253854@qq.com>
2018-06-11 00:41:09 +0000

    baodao

--- src/main/java/CoreJavaVolume/Role1/Character3/CodePointBasic.java
+++ src/main/java/CoreJavaVolume/Role1/Character3/CodePointBasic.java
@@ -1,5 +1,9 @@
 package CoreJavaVolume.Role1.Character3;
 
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.*;
 
 /**
@@ -16,27 +20,42 @@ public class CodePointBasic {
      */
     public static void main(String[] args) {
 
-        System.out.println("请初始化你的字符串");
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        //代码单元数量即实际长度
-        int strCodePointCount = str.codePointCount(0,str.length());
-        //得到指定i的码点
-        System.out.print("该字符串实际长度:" +strCodePointCount + ",请输入你需要检索的码点:");
-        int i = scanner.nextInt();
-        int index = str.offsetByCodePoints(0,i);
-        int cp = str.codePointAt(index);
-        System.out.println("对应代码单元:" + str.charAt(index));
-        System.out.println("对应码点的流值:" + cp);
+//        System.out.println("请初始化你的字符串");
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.nextLine();
+//        //代码单元数量即实际长度
+//        int strCodePointCount = str.codePointCount(0,str.length());
+//        //得到指定i的码点
+//        System.out.print("该字符串实际长度:" +strCodePointCount + ",请输入你需要检索的码点:");
+//        int i = scanner.nextInt();
+//        int index = str.offsetByCodePoints(0,i);
+//        int cp = str.codePointAt(index);
+//        System.out.println("对应代码单元:" + str.charAt(index));
+//        System.out.println("对应码点的流值:" + cp);
+//
+//        //遍历字符串时将字符串转换为流数组
+//        int[] codePoints = str.codePoints().toArray();
+//        i = 0;
+//        //遍历指定码点的流的int值
+//        while(i<str.codePointCount(0,str.length())) {
+//            System.out.print(str.charAt(str.offsetByCodePoints(0,i)) + "的是:" + codePoints[i++] + "\t");
+//        }
+//        //把码点数组转为字符串
+//        String string = new String(codePoints,0,codePoints.length);
+//        System.out.println("\n" + string);
+//        //格式化日期，java.time相关方法
+//        System.out.printf("%tc",new Date());
 
-        //遍历字符串时将字符串转换为流数组
-        int[] codePoints = str.codePoints().toArray();
-        i = 0;
-        //遍历指定码点的流的int值
-        while(i<str.codePointCount(0,str.length())) {
-            System.out.print(str.charAt(str.offsetByCodePoints(0,i)) + "的是:" + codePoints[i++] + "\t");
-        }
-        //把码点数组转为字符串
-        String string = new String(codePoints,0,codePoints.length);
+//        BigInteger value = BigInteger.valueOf(130L);
+//        BigInteger result = value.mod(BigInteger.valueOf(10L));
+//        System.out.printf(result.toString());
+//
+//        BigDecimal bigDecimal = BigDecimal.valueOf(1296.00);
+//        BigDecimal bigDecimal11 = bigDecimal.divide(BigDecimal.valueOf(10), RoundingMode.HALF_UP);
+//        System.out.println(bigDecimal11.toString().trim());
+
+//        int[] array = {1,23,5,6,7,8,2};
+//        System.out.println(Arrays.toString(array));
+//        int[] array = new int[0];
     }
 }
